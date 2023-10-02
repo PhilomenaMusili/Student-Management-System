67a42b316a193b7623a02529cda937a3d0d5297f
PhilomenaMusili <philomenamusili@gmail.com>
2023-10-02 16:52:16 +0300

    another commit

--- Student_profile.php
+++ Student_profile.php
@@ -10,6 +10,33 @@ if(!isset($_SESSION['username']))
 elseif($_SESSION['usertype']=='admin'){
     header("location:login.php");
 }
+    $host ="localhost";
+    $username ="root";
+    $password="";
+    $db ="school_project";
+    
+    $data = mysqli_connect($host,$username,$password,$db);
+    $name =$_SESSION['username'];
+    $sql ="SELECT * FROM user WHERE username='$name' ";
+    $result = mysqli_query($data,$sql);
+    $info = mysqli_fetch_assoc($result);
+    if(isset($_POST['Update_profile']))
+    {
+        $s_email=$_POST['email'];
+        $s_phone=$_POST['phone'];
+        $s_password=$_POST['password'];
+
+        $sql2 ="UPDATE user SET email='$s_email',phone='$s_phone',
+        password='$s_password' WHERE username='$name' ";
+
+        $result2 =mysqli_query($data,$sql2);
+
+        if($result2)
+        {
+            header('location:student_profile.php');
+        }
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -51,26 +78,26 @@ include 'student_sidebar.php';
    <center>
     <h1> Update profile</h1>
     <br><br>
-    <form>
+    <form action="#" method="POST">
     <div class="div_design">
+        
         <div>
-            <label>Name</label>
-            <input type="text" name="name">
-        </div>
-        <div>
-            <label>Email</label>
-            <input type="email" name="email">
+        <label>Email</label>
+            <input type="email" name="email"
+            value="<?php echo "{$info['email']}" ?>">
         </div>
         <div>
             <label>Phone</label>
-            <input type="number" name="phone">
+            <input type="number" name="phone"
+            value="<?php echo "{$info['phone']}" ?>">
         </div>
         <div>
             <label>Password</label>
-            <input type="text" name="password">
+            <input type="text" name="password"
+            value="<?php echo "{$info['password']}" ?>">
         </div>
         <div>
-            <input type="submit" class="btn btn-primary"name="Update_profile">
+            <input type="submit" class="btn btn-primary"name="Update_profile" value="Update">
         </div>
     </div>
   </form>
